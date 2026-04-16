0f8673ae769f1b5f8c1a35d0db1235315561f51f
Sanjivani Bhongade <70435997+sanju200@users.noreply.github.com>
2026-04-17 01:35:25 +0530

    updated default value for category

--- src/category/category.entity.ts
+++ src/category/category.entity.ts
@@ -13,16 +13,16 @@ export class Category {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @Column({ type: 'varchar', length: 100, nullable: true })
+    @Column({ type: 'varchar', length: 100, default: 'General' })
     category: string;
 
-    @Column({ type: 'varchar', length: 255 })
+    @Column({ type: 'varchar', length: 255, default: 'General' })
     title: string;
 
-    @Column({ type: 'varchar', length: 50 })
+    @Column({ type: 'varchar', length: 50, default: '🥬' })
     icon: string;
 
-    @Column({ type: 'varchar', length: 50 })
+    @Column({ type: 'varchar', length: 50, default: '#ffffff' })
     bgColor: string;
 
     @CreateDateColumn({ type: 'timestamp' })
